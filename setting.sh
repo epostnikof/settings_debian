6b736afc6785918595cdfbf97f50e59db8911c9f
epostnikof <epostnikof@yandex.ru>
2022-04-04 12:57:49 +0300

    edit setting.sh

--- setting.sh
+++ setting.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir /opt/scripts
-path=/opt/scripts/if.sh
-cp if.sh $path
-cp up-interface.service  /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable up-interface.service
+echo "Нужно ли настроить автоматически интерфейсы? [y or n?]"
+read answer1
+if [[ "$answer1" == "y" || "$answer1" = "Y" ]]; then
+	mkdir /opt/scripts
+	path=/opt/scripts/if.sh
+	cp if.sh $path
+	cp up-interface.service  /etc/systemd/system/up-interface.service
+	systemctl daemon-reload
+	systemctl enable up-interface.service
+	echo "Все сетевые интерфейсы настроены в автоматическом режиме"
+else
+	echo "Значит всё работает как надо"
+fi
 
 echo "Нужно ли менять имя машины? [y or n?]"
 read answer
@@ -18,6 +25,6 @@ if [[ "$answer" == "y" || "$answer" = "Y" ]]; then
 	echo $name > /etc/hostname
 	echo "Для применения новых настроек нужно перезагрузить сервер"
 	exit
-else 
+else
 	echo "Ну и ладно"
 fi
